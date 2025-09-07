6298adc1f02c41378a564c2a68ddf025630bc88c
DewiKadek <kadekpratiwisukmadewi@tilburguniversity.edu>
2025-09-07 17:59:25 +0200

    Created a messy file (with help from GenAI) to test my newly added Ruff configurations

---
+++ test.py
@@ -0,0 +1,14 @@
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+def say_hello(name):
+    print("Hello, " + name)
+
+
+if __name__ == "__main__":
+    x = add_numbers(1, 2)
+    say_hello("World")
+    print("Sum:", x)
